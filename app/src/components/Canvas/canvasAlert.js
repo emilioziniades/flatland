3d6c729ad6ae3f3abe99a35f1212c63b4455e9c3
Emilio <emilioziniades@gmail.com>
2021-04-20 23:37:43 +0200

    canvasAlert is dismissible

--- app/src/components/Canvas/canvasAlert.js
+++ app/src/components/Canvas/canvasAlert.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Row, Alert} from 'react-bootstrap'
 
 const CanvasAlert = () => {
+
+    const [show, setShow] = useState(true)
+    
+    const handleClose = () => setShow(false)
+
     return(
         <Row className='d-flex justify-content-center'>
-        <Alert variant='warning' className='pr-5 pl-5'>
-            Note: transactions claiming a square or changing colour must be confirmed on the Ethereum Ropsten Testnet blockchain before it will reflect on Flatland. Blocks are mined every 15 seconds on average.
-        </Alert>
-    </Row>
+        { show && (
+            <Alert variant='warning' className='pr-5 pl-5' onClose={handleClose} dismissible>
+                Note: transactions claiming a square or changing colour must be confirmed on the Ethereum Ropsten Testnet blockchain before it will reflect on Flatland. Blocks are mined every 15 seconds on average.
+            </Alert>
+            )}
+        </Row>
     )
 }
 
